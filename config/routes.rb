baadb14a36ee63c5f1b98c87413e033137fa9aeb
Samir Zeka <sam_ned00@hotmail.com>
2011-12-13 12:34:11 +0100

    Adde layout links (with routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
   get "pages/home"
 
-  get "pages/contact"
-  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -46,5 +46,7 @@ describe PagesController do
       get "about"
       response.should have_selector("title", :content => "#{@base_title} | About")
     end
+    
+    
   end
 end
